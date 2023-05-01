041ea35f25ddbad82121de81acf003abace3b6e2
Juan Sebastian Quirogs <101578588+jsebastianquiroga@users.noreply.github.com>
2023-05-01 11:54:37 -0500

    Add files via upload

--- Final/frontera_v4.py
+++ Final/frontera_v4.py
@@ -878,24 +878,24 @@ class Frontera:
             labels_reduct = self.label
             return vec_redu, labels_reduct
 
-        # elif self.method == 'prototypes':
-        #     self.get_frontier()
-        #     self.get_X1_Y1()
-        #     self.fit()
-        #     # assign return values to variables
-        #     w_ = self.w_
-        #     c_w_ = self.c_w_
-        #     return w_, c_w_
+        elif self.method == 'prototypes':
+            self.get_frontier()
+            self.get_X1_Y1()
+            self.fit()
+            # assign return values to variables
+            w_ = self.w_
+            c_w_ = self.c_w_
+            return w_, c_w_
 
-        # elif self.method == 'LSH':
-        #     self.get_frontier()
-        #     self.get_X1_Y1()
-        #     self.create_groups()
-        #     self.fit_lsh()
-        #     # assign return values to variables
-        #     centroid_arrays = self.centroids
-        #     centroid_median_labels = self.centroid_label
-        #     return centroid_arrays, centroid_median_labels
+        elif self.method == 'LSH':
+            self.get_frontier()
+            self.get_X1_Y1()
+            self.create_groups()
+            self.fit_lsh()
+            # assign return values to variables
+            centroid_arrays = self.centroids
+            centroid_median_labels = self.centroid_label
+            return centroid_arrays, centroid_median_labels
 
         # elif self.method == 'KM':
         #     self.get_frontier()
